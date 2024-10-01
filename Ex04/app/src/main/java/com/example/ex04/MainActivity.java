2dd0fd5c3526359bfbc095a6de4a3e452370de23
Do_Huy_Hoang <hoangdo18022003@gmail.com>
2024-10-01 23:57:11 +0700

    build layout and activity Ex04

---
+++ Ex04/app/src/main/java/com/example/ex04/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.ex04;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edtdoC, edtdoF;
+    Button btncf, btnfc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtdoC = findViewById(R.id.edtCel);
+        edtdoF = findViewById(R.id.edtFah);
+        btncf = findViewById(R.id.btncf);
+        btnfc = findViewById(R.id.btnfc);
+        btncf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doC = edtdoC.getText() + "";
+                int C = Integer.parseInt(doC);
+                edtdoF.setText("" + dcf.format(C * 1.8 + 32));
+            }
+        });
+        btnfc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DecimalFormat dcf = new DecimalFormat("#.00");
+                String doF = edtdoF.getText() + "";
+                int F = Integer.parseInt(doF);
+                edtdoC.setText("" + dcf.format((F - 32) / 1.8));
+            }
+        });
+    }
+}
