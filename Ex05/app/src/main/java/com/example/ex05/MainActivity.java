599421f1c62a5cd4e61b9ab59375509557c997e6
Do_Huy_Hoang <hoangdo18022003@gmail.com>
2024-10-21 12:47:23 +0700

    build layout

---
+++ Ex05/app/src/main/java/com/example/ex05/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.ex05;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+}
