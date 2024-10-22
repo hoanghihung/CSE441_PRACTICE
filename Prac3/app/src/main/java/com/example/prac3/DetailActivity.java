eeeb6486cfe54368f50d11e173ae2ab126b27da3
Do_Huy_Hoang <hoangdo18022003@gmail.com>
2024-10-23 01:38:16 +0700

    build activity prac3

--- Prac3/app/src/main/java/com/example/prac3/DetailActivity.java
+++ Prac3/app/src/main/java/com/example/prac3/DetailActivity.java
@@ -1,6 +1,41 @@
 package com.example.prac3;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class DetailActivity extends Activity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.prac3.Adapter.Country;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ImageView flagImageViewDetail;
+    private TextView nameTextViewDetail, capitalTextViewDetail, populationTextViewDetail, areaTextViewDetail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        flagImageViewDetail = findViewById(R.id.flagImageViewDetail);
+        nameTextViewDetail = findViewById(R.id.nameTextViewDetail);
+        capitalTextViewDetail = findViewById(R.id.capitalTextViewDetail);
+        populationTextViewDetail = findViewById(R.id.populationTextViewDetail);
+        areaTextViewDetail = findViewById(R.id.areaTextViewDetail);
+
+        Country country = getIntent().getParcelableExtra("country");
+
+        if (country != null) {
+            flagImageViewDetail.setImageResource(country.getFlagResourceId());
+            nameTextViewDetail.setText(country.getName());
+            capitalTextViewDetail.setText("Capital: " + country.getCapital());
+            populationTextViewDetail.setText("Population: " + country.getPopulation());
+            areaTextViewDetail.setText("Area: " + country.getArea() + " km²");
+        }
+    }
 }
--- Prac3/app/src/main/java/com/example/prac3/MainActivity.java
+++ Prac3/app/src/main/java/com/example/prac3/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.prac3;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.prac3.Adapter.Country;
+import com.example.prac3.Adapter.CountryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CountryAdapter adapter;
+    private List<Country> countryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        countryList = new ArrayList<>();
+        countryList.add(new Country("India", "New Delhi", 1428600000, 2973190, R.drawable.india));
+        countryList.add(new Country("China", "Beijing", 1411800000, 9596961, R.drawable.vn));
+        // Thêm nhiều quốc gia khác
+
+        adapter = new CountryAdapter(countryList, country -> {
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            intent.putExtra("country", country);
+            startActivity(intent);
         });
+        recyclerView.setAdapter(adapter);
     }
 }
+
+
